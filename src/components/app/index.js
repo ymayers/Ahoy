bfbd6cacc4e668151031211d744ad884e39a7704
Yolea Mayers <yoleamayers@yoleas-air.home>
2020-06-30 15:41:55 -0400

    successfully retrieved data from api

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,17 +1,18 @@
-import * as css from './index.css'
+import * as css from "./index.css";
+import 'regenerator-runtime/runtime';
 
 export default class App {
-    constructor (elem) {
-      if (!elem) return
-      this.elem = elem
-    }
+  constructor(elem) {
+    if (!elem) return;
+    this.elem = elem;
+  }
 
-    render () {
-        if (this.elem) this.elem.innerHTML = `
-        <section data-component="app">
-        <h1>App Component</h1>  
-        <p>The time is: ${(new Date()).toLocaleString()}</p>
+  render() {
+    if (this.elem)
+      this.elem.innerHTML = `
+        <h1>App Component</h1>
+        <p>The time is: ${new Date().toLocaleString()}</p>
         </section>
-        `
-    }
+        `;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,49 @@
  * The entry point
  */
 
-import App from './components/app'
+// import App from "./components/app";
+// import axios from 'axios';
+import "regenerator-runtime/runtime.js";
+import "babel-polyfill";
 
-window.addEventListener('load', () => {
-    const app = new App(document.getElementById('app'))
+// window.addEventListener("load", async () => {
+//   const app = new App(document.getElementById("app"));
+const APIKEY = `17251237-ace6a816706b339ed6426cdb5`;
+const url = `https://pixabay.com/api/?key=${APIKEY}&q=ships`;
+//   let response = await axios.get(`https://pixabay.com/api/?key=APIKEY&q=ships`);
+// const ships = response.data;
+// console.log(ships);
 
-    // A very simple component setup
-    app.render()
+// async function getData() {
+//   const ships = await fetch(`${url}`, {
+//     method: "GET",
+//     mode: "no-cors",
+//   }).then((response) => {
+//   return response.json();
+// })
+// .then((ships) => {
+//   console.log(ships);
+// })
+// .catch((err) => {
+//   console.error(err);
+// });
 
-    // Render the time every 1s
-    setInterval(() => {
-      app.render()
-    }, 1000)
+async function getData() {
+  let response = await fetch(`${url}`);
+  let data = await response.json();
+  return data;
+}
+getData().then((data) => console.log(data));
+// .catch((err) => {
+//   console.error(err);
+// });
 
-})
+// A very simple component setup
+// app.render();
+
+// Render the time every 1s
+//   setInterval(() => {
+//     app.render();
+//   }, 1000);
+
+// });
